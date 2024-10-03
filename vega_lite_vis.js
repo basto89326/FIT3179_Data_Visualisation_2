56f711113d01440385c6b1d5466e96ec7a26e96a
basto89326 <jbas0020@student.monash.edu>
2024-10-03 20:34:45 +1000

    Changed csv file to long format, and added grouped bar chart to html

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,4 +1,12 @@
 var vg_1 = "top_tourist_destinations_map.json";
+var vg_2 = "tourism_incoming_2019_2024.json";
+
+// Top tourist destinations map
 vegaEmbed("#choropleth_map", vg_1).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+// Pre vs Post COVID tourism numbers grouped bar chart
+vegaEmbed("#grouped_bar", vg_2).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
